1e96cc3c918204b845285d11bbfbc82df5639b0a
Harish H J <harishjayram95@gmail.com>
2025-12-31 17:13:12 +0530

    new updated for emp list , update , delete , add

--- src/components/EmpCreate.jsx
+++ src/components/EmpCreate.jsx
@@ -1,7 +1,86 @@
+// import React, { useState } from 'react'
+// import "./empcreate.css";
+// import { Link, useNavigate } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+// const EmpCreate = () => {
+//     const[name , setName]=useState("");
+//     const[email , setEmail]=useState("");
+//     const[phone , setPhone]=useState("");
+//     const[gender , setGender]=useState("");
+
+
+//     const navigate = useNavigate();
+
+//     let handleGenderChange = e=>{
+//         if(e.target.checked == true)
+//         {
+//             setGender(e.target.value);
+//         }
+//     };
+
+//     let handleSubmit = e=>{
+//         e.preventDefault();
+//         // console.log(name , email , phone , gender);
+//         let payload ={name , email , phone , gender};
+
+//         window.fetch("http://localhost:8000/employees",{
+//             method:"POST",
+//             body:JSON.stringify(payload)
+//         }).then(()=>{toast.success("employee data created successfully");
+//             navigate("/");
+//         })
+//         .catch(()=>toast.error("employee data not created"));
+//     }
+//   return (
+//     <div className="mainBlock">
+//         <section id='formBlock'>
+
+//         <article>
+//             <h1>Create An Employee</h1>
+//             <form onSubmit={handleSubmit}>
+//                 <input type="text" placeholder='enter username' 
+//                 className='boxset'
+//                 value={name}
+//                 onChange={e=>setName(e.target.value)}
+//                 />
+//                 <br />
+//                 <input type="email" placeholder='enter email'
+//                 className='boxset'
+//                  value={email}
+//                 onChange={e=>setEmail(e.target.value)}
+//                 />
+//                 <br />
+//                 <input type="tel" placeholder='enter phone number'
+//                 className='boxset'
+//                  value={phone}
+//                 onChange={e=>setPhone(e.target.value)}
+//                 />
+//                 <br />
+//                 <div className="radioAndLabel">
+//                 <input type="radio" name='gender' value="male" onChange={handleGenderChange}/>Male 
+//                 <input type="radio" name='gender' value="female" onChange={handleGenderChange}/>Female 
+//                 <input type="radio" name='gender' value="others" onChange={handleGenderChange}/>Others
+//                 </div>
+//                 <br />
+//                 <input type="submit" value="Create Employee" className='submitting'/>
+//                 <Link to="/" className='backhome'>Back to Home page</Link>
+//             </form>
+//         </article>
+//     </section>
+//     </div>
+//   )
+// }
+
+// export default EmpCreate
+
+
+
+
 import React, { useState } from 'react'
 import "./empcreate.css";
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 const EmpCreate = () => {
     const[name , setName]=useState("");
     const[email , setEmail]=useState("");
@@ -23,10 +102,8 @@ const EmpCreate = () => {
         // console.log(name , email , phone , gender);
         let payload ={name , email , phone , gender};
 
-        window.fetch("http://localhost:8000/employees",{
-            method:"POST",
-            body:JSON.stringify(payload)
-        }).then(()=>{toast.success("employee data created successfully");
+        axios.post("http://localhost:8000/employees" , payload)
+        .then(()=>{toast.success("employee data created successfully");
             navigate("/");
         })
         .catch(()=>toast.error("employee data not created"));
--- src/components/EmpDetails.jsx
+++ src/components/EmpDetails.jsx
@@ -1,3 +1,39 @@
+// import React, { useEffect, useState } from 'react'
+// import { Link, useParams } from 'react-router-dom';
+// import { toast } from 'react-toastify';
+
+// const EmpDetails = () => {
+//   const[emp , setEmp] = useState({});
+//   // console.log(emp);
+//   const {eid} = useParams();
+//   useEffect(()=>{
+//     fetch("http://localhost:8000/employees/"+eid)
+//     .then(res=>res.json().then(data=>{
+//       // console.log(data);
+//       setEmp(data);
+//     }))
+//     .catch(error=>toast.warning("employee not found"))
+//   },[])
+
+//   return (
+//     <div>
+//       <h1>View Employee Data</h1>
+//       <hr />
+//       <h2>Employee Name : {emp.name}</h2>
+//       <h2>Employee Email : {emp.email}</h2>
+//       <h2>Employee Phone : {emp.phone}</h2>
+//       <h2>Employee Gender : {emp.gender}</h2>
+//       <Link to='/'>Back to Homepage </Link>
+//     </div>
+//   )
+// }
+
+// export default EmpDetails
+
+
+
+
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -7,11 +43,10 @@ const EmpDetails = () => {
   // console.log(emp);
   const {eid} = useParams();
   useEffect(()=>{
-    fetch("http://localhost:8000/employees/"+eid)
-    .then(res=>res.json().then(data=>{
-      // console.log(data);
-      setEmp(data);
-    }))
+    axios.get("http://localhost:8000/employees/"+eid)
+    .then(res=>{
+      setEmp(res.data);
+    })
     .catch(error=>toast.warning("employee not found"))
   },[])
 
